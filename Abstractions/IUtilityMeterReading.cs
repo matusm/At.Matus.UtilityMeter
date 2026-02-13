2e898abe552866035025b16f32fbedd88d46a4ca
Michael Matus <michael@matus.at>
2026-02-13 21:32:27 +0100

    Add IUtilityMeterReading interface and electricity reading
    Introduce IUtilityMeterReading abstraction for utility meter readings, defining common properties and methods. Add ElectricityMeterReading class as a concrete implementation for electricity meters. Update project file to include new interface and class, enabling standardized handling of meter readings.

---
+++ Abstractions/IUtilityMeterReading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace At.Matus.UtilityMeter
+{
+    public interface IUtilityMeterReading : IComparable<IUtilityMeterReading>
+    {
+        DateTime TimeStamp { get; }
+        double Reading { get; }
+        string Unit { get; }
+        string MeterID { get; }
+        string Comment { get; }
+        
+        string GetCsvLine();
+        bool IsSameMeter(IUtilityMeterReading other);
+    }
+}
---
+++ Domain/ElectricityMeterReading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace At.Matus.UtilityMeter
+{
+    public class ElectricityMeterReading : IUtilityMeterReading
+    {
+        public DateTime TimeStamp { get; }
+        public double Reading { get; }
+        public string Unit { get; }
+        public string MeterID { get; }
+        public string Comment { get; }
+
+        DateTime IUtilityMeterReading.TimeStamp => throw new NotImplementedException();
+
+        double IUtilityMeterReading.Reading => throw new NotImplementedException();
+
+        string IUtilityMeterReading.Unit => throw new NotImplementedException();
+
+        string IUtilityMeterReading.MeterID => throw new NotImplementedException();
+
+        string IUtilityMeterReading.Comment => throw new NotImplementedException();
+
+        public ElectricityMeterReading(DateTime timeStamp, double reading, string meterID) : this(timeStamp, reading, meterID, string.Empty){}
+
+        public ElectricityMeterReading(DateTime timeStamp, double reading, string meterID, string comment)
+        {
+            Unit = "kWh";
+            TimeStamp = timeStamp;
+            Reading = reading;
+            MeterID = meterID.Trim();
+            Comment = comment.Trim();
+        }
+
+        public string GetCsvLine() => $"{TimeStamp.ToString("dd-MM-yyyy HH:mm")};{Reading};{Unit},{MeterID};{Comment}";
+
+        public int CompareTo(IUtilityMeterReading other) => TimeStamp.CompareTo(other.TimeStamp);
+
+        public bool IsSameMeter(IUtilityMeterReading other)
+        {
+            return string.Equals(other.MeterID, MeterID, StringComparison.InvariantCultureIgnoreCase); //TODO probably case should matter
+        }
+    }
+}
